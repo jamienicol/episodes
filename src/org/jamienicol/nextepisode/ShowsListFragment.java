ae4295c510cabf7e7c86dd3f3b023f7826f5cc8c
Jamie Nicol <jamie@thenicols.net>
2012-12-25 22:26:21 +0000

    Create (empty) shows list fragment
    Create ShowsListFragment as an empty ListFragment.
    In the future this will read shows from the database and display them.
    For the time being it does nothing.

---
+++ src/org/jamienicol/nextepisode/ShowsListFragment.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2012 Jamie Nicol <jamie@thenicols.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jamienicol.nextepisode;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShowsListFragment extends ListFragment
+{
+	public View onCreateView(LayoutInflater inflater,
+	                         ViewGroup container,
+	                         Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.shows_list_fragment, container, false);
+	}
+}
