0b3ce741b6bb4f88ca7fc91fe9cfefe53c634355
Jamie Nicol <jamie@thenicols.net>
2013-06-11 22:16:32 +0100

    ui: show progress bars in shows list
    they are ugly. need to be made prettier

--- src/org/jamienicol/episodes/ShowsListFragment.java
+++ src/org/jamienicol/episodes/ShowsListFragment.java
@@ -18,21 +18,26 @@
 package org.jamienicol.episodes;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import java.util.HashMap;
+import org.jamienicol.episodes.db.EpisodesTable;
 import org.jamienicol.episodes.db.ShowsProvider;
 import org.jamienicol.episodes.db.ShowsTable;
 import org.jamienicol.episodes.services.RefreshShowService;
@@ -40,8 +45,12 @@ import org.jamienicol.episodes.services.RefreshShowService;
 public class ShowsListFragment extends SherlockListFragment
 	implements LoaderManager.LoaderCallbacks<Cursor>
 {
-	private SimpleCursorAdapter listAdapter;
+	private static final int LOADER_ID_SHOWS = 0;
+	private static final int LOADER_ID_EPISODES = 1;
+
+	private ShowsListAdapter listAdapter;
 	private Cursor showsData;
+	private Cursor episodesData;
 
 	public interface OnShowSelectedListener {
 		public void onShowSelected(int showId);
@@ -86,15 +95,13 @@ public class ShowsListFragment extends SherlockListFragment
 			R.id.show_name_view
 		};
 
-		listAdapter = new SimpleCursorAdapter(getActivity(),
-		                                      R.layout.shows_list_item,
-		                                      null,
-		                                      from,
-		                                      to,
-		                                      0);
+		listAdapter = new ShowsListAdapter(getActivity(),
+		                                   null,
+		                                   null);
 		setListAdapter(listAdapter);
 
-		getLoaderManager().initLoader(0, null, this);
+		getLoaderManager().initLoader(LOADER_ID_SHOWS, null, this);
+		getLoaderManager().initLoader(LOADER_ID_EPISODES, null, this);
 	}
 
 	@Override
@@ -126,27 +133,62 @@ public class ShowsListFragment extends SherlockListFragment
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		String[] projection = {
-			ShowsTable.COLUMN_ID,
-			ShowsTable.COLUMN_NAME
-		};
-		return new CursorLoader(getActivity(),
-		                        ShowsProvider.CONTENT_URI_SHOWS,
-		                        projection,
-		                        null,
-		                        null,
-		                        ShowsTable.COLUMN_NAME + " ASC");
+		if (id == LOADER_ID_SHOWS) {
+			String[] projection = {
+				ShowsTable.COLUMN_ID,
+				ShowsTable.COLUMN_NAME
+			};
+			return new CursorLoader(getActivity(),
+			                        ShowsProvider.CONTENT_URI_SHOWS,
+			                        projection,
+			                        null,
+			                        null,
+			                        ShowsTable.COLUMN_NAME + " ASC");
+
+		} else if (id == LOADER_ID_EPISODES) {
+			String[] projection = {
+				EpisodesTable.COLUMN_SHOW_ID,
+				EpisodesTable.COLUMN_WATCHED
+			};
+			return new CursorLoader(getActivity(),
+			                        ShowsProvider.CONTENT_URI_EPISODES,
+			                        projection,
+			                        null,
+			                        null,
+			                        null);
+
+		} else {
+			throw new IllegalArgumentException("invalid loader id");
+		}
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		showsData = data;
+		switch (loader.getId()) {
+		case LOADER_ID_SHOWS:
+			showsData = data;
+			break;
+
+		case LOADER_ID_EPISODES:
+			episodesData = data;
+			break;
+		}
+
 		refreshViews();
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
-		showsData = null;
+		switch (loader.getId()) {
+		case LOADER_ID_SHOWS:
+			showsData = null;
+			break;
+
+		case LOADER_ID_EPISODES:
+			episodesData = null;
+			break;
+		}
+
 		refreshViews();
 	}
 
@@ -158,7 +200,8 @@ public class ShowsListFragment extends SherlockListFragment
 	// ensures views are updated to display newest data.
 	// to be called whenever a new cursor has been loaded
 	private void refreshViews() {
-		listAdapter.swapCursor(showsData);
+		listAdapter.swapShowsCursor(showsData);
+		listAdapter.swapEpisodesCursor(episodesData);
 
 		// force a new decision on whether to display certain menu items
 		getActivity().supportInvalidateOptionsMenu();
@@ -179,6 +222,137 @@ public class ShowsListFragment extends SherlockListFragment
 				getActivity().startService(intent);
 
 			} while (showsData.moveToNext());
+		}
+	}
+
+	private static class ShowsListAdapter
+		extends BaseAdapter
+	{
+		private Context context;
+		private HashMap<Integer, Integer> numEpisodesMap;
+		private HashMap<Integer, Integer> numWatchedEpisodesMap;
+		private Cursor showsCursor;
+
+		public ShowsListAdapter(Context context,
+		                        Cursor showsCursor,
+		                        Cursor episodesCursor) {
+			this.context = context;
+
+			countEpisodes(episodesCursor);
+		}
+
+		public void swapShowsCursor(Cursor showsCursor) {
+			this.showsCursor = showsCursor;
+
+			notifyDataSetChanged();
+		}
+
+		public void swapEpisodesCursor(Cursor episodesCursor) {
+			countEpisodes(episodesCursor);
+
+			if (showsCursor != null) {
+				notifyDataSetChanged();
+			}
+		}
+
+		private void countEpisodes(Cursor episodesCursor) {
+			numEpisodesMap = new HashMap<Integer, Integer>();
+			numWatchedEpisodesMap = new HashMap<Integer, Integer>();
+
+			/* extract the total number and number of watched episodes
+			 * for each show. */
+			if (episodesCursor != null && episodesCursor.moveToFirst()) {
+				do {
+					int showIdColumnIndex =
+						episodesCursor.getColumnIndexOrThrow(EpisodesTable.COLUMN_SHOW_ID);
+					int showId = episodesCursor.getInt(showIdColumnIndex);
+
+					// ensure entries exist in maps for this show id
+					if (numEpisodesMap.containsKey(showId) == false) {
+						numEpisodesMap.put(showId, 0);
+					}
+					if (numWatchedEpisodesMap.containsKey(showId) == false) {
+						numWatchedEpisodesMap.put(showId, 0);
+					}
+
+					// increment num episodes for this show
+					numEpisodesMap.put(showId, numEpisodesMap.get(showId) + 1);
+
+					// if episode is watched, increment value for this show
+					int watchedColumnIndex =
+						episodesCursor.getColumnIndexOrThrow(EpisodesTable.COLUMN_WATCHED);
+					boolean watched =
+						episodesCursor.getInt(watchedColumnIndex) > 0 ? true : false;
+					if (watched) {
+						numWatchedEpisodesMap.put(showId,
+						                          numWatchedEpisodesMap.get(showId) + 1);
+					}
+				} while (episodesCursor.moveToNext());
+			}
+		}
+
+		@Override
+		public int getCount() {
+			if (showsCursor == null) {
+				return 0;
+			} else {
+				return showsCursor.getCount();
+			}
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			showsCursor.moveToPosition(position);
+
+			int idColumnIndex =
+				showsCursor.getColumnIndexOrThrow(ShowsTable.COLUMN_ID);
+			return showsCursor.getInt(idColumnIndex);
+		}
+
+		@Override
+		public View getView(int position,
+		                    View convertView,
+		                    ViewGroup parent) {
+
+			LayoutInflater inflater = LayoutInflater.from(context);
+			if(convertView == null) {
+				convertView = inflater.inflate(R.layout.shows_list_item,
+				                               parent,
+				                               false);
+			}
+
+			showsCursor.moveToPosition(position);
+
+			int idColumnIndex =
+				showsCursor.getColumnIndexOrThrow(ShowsTable.COLUMN_ID);
+			int id = showsCursor.getInt(idColumnIndex);
+
+			TextView nameView =
+				(TextView)convertView.findViewById(R.id.show_name_view);
+			int nameColumnIndex =
+				showsCursor.getColumnIndexOrThrow(ShowsTable.COLUMN_NAME);
+			String name = showsCursor.getString(nameColumnIndex);
+			nameView.setText(name);
+
+			ProgressBar progressBar =
+				(ProgressBar)convertView.findViewById(R.id.show_progress_bar);
+			int numEpisodes = 0;
+			if (numEpisodesMap.containsKey(id)) {
+				numEpisodes = numEpisodesMap.get(id);
+			}
+			int numWatched = 0;
+			if (numWatchedEpisodesMap.containsKey(id)) {
+				numWatched = numWatchedEpisodesMap.get(id);
+			}
+			progressBar.setMax(numEpisodes);
+			progressBar.setProgress(numWatched);
+
+			return convertView;
 		}
 	}
 }
