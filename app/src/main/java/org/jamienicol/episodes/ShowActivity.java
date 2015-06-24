b1c612b4846e35f4ccb504cd40b6171b059eadb3
Daniele Ricci <daniele.athome@gmail.com>
2015-06-24 11:58:47 +0200

    Personal notes linked to shows (UI; read-only)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- app/src/main/java/org/jamienicol/episodes/ShowActivity.java
+++ app/src/main/java/org/jamienicol/episodes/ShowActivity.java
@@ -354,7 +354,7 @@ public class ShowActivity
 
 		@Override
 		public int getCount() {
-			return 2;
+			return 3;
 		}
 
 		@Override
@@ -364,6 +364,8 @@ public class ShowActivity
 				return context.getString(R.string.show_tab_overview);
 			case 1:
 				return context.getString(R.string.show_tab_episodes);
+			case 2:
+				return context.getString(R.string.show_tab_notes);
 			default:
 				return null;
 			}
@@ -376,6 +378,8 @@ public class ShowActivity
 				return ShowDetailsFragment.newInstance(showId);
 			case 1:
 				return SeasonsListFragment.newInstance(showId);
+			case 2:
+				return ShowNotesFragment.newInstance(showId);
 			default:
 				return null;
 			}
